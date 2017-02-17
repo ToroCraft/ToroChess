1d5334dfc1c28496d1462bcc021fb4ea42d93a61
Charles Howard <frodare@gmail.com>
2017-02-16 21:55:36 -0500

    adding icons texture and overlay code

--- java/net/torocraft/chess/CheckerBoardOverlay.java
+++ java/net/torocraft/chess/CheckerBoardOverlay.java
@@ -31,7 +31,8 @@ public class CheckerBoardOverlay {
 	private static double T = 0.125f;
 
 	private List<SelectBlock> selectedBlocks = new ArrayList<>();
-	private static final ResourceLocation TEXTURE = new ResourceLocation(ToroChess.MODID, "textures/overlay.png");
+	private static final ResourceLocation LOCATIONS_TEXTURE = new ResourceLocation(ToroChess.MODID, "textures/overlay.png");
+	private static final ResourceLocation ICONS_TEXTURE = new ResourceLocation(ToroChess.MODID, "textures/icons.png");
 
 	static {
 		for (int i = 0; i < 8; i++) {
@@ -87,6 +88,9 @@ public class CheckerBoardOverlay {
 		if (offset.getX() > 7 || offset.getX() < 0 || offset.getZ() > 7 || offset.getZ() < 0 || offset.getY() != 0) {
 			return;
 		}
+
+		b.valid = offset.getX() == 0;
+
 		b.u = 7 - offset.getX();
 		b.v = offset.getZ();
 		b.pos = r.getBlockPos();
@@ -98,31 +102,50 @@ public class CheckerBoardOverlay {
 			return;
 		}
 		TextureManager tm = Minecraft.getMinecraft().renderEngine;
-		tm.bindTexture(TEXTURE);
-		VertexBuffer vb = Tessellator.getInstance().getBuffer();
 		GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
 		GL11.glPushMatrix();
 		GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 		GL11.glEnable(GL11.GL_BLEND);
 		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-		vb.setTranslation(-x, -y, -z);
-		for (SelectBlock u : selectedBlocks) {
-			renderVectors(vb, u, side);
-		}
-		vb.setTranslation(0, 0, 0);
-		Tessellator.getInstance().draw();
+
+		drawLocationVectors(x, y, z, side, tm);
+		drawIconVectors(x, y, z, side, tm);
+
 		GL11.glPopMatrix();
 		GL11.glPopAttrib();
 	}
 
-	private void renderVectors(VertexBuffer vb, SelectBlock info, Side side) {
-		double x = info.pos.getX();
-		double y = info.pos.getY() + 1.001;
-		double z = info.pos.getZ();
+	private void drawLocationVectors(double x, double y, double z, Side side, TextureManager tm) {
+		tm.bindTexture(LOCATIONS_TEXTURE);
+		VertexBuffer vb = Tessellator.getInstance().getBuffer();
+		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
+		vb.setTranslation(-x, -y, -z);
+		for (SelectBlock select : selectedBlocks) {
+			renderVectors(vb, select.pos, TEXTURE_OFFSETS[select.u], TEXTURE_OFFSETS[select.v], side, 1.002);
+		}
+		vb.setTranslation(0, 0, 0);
+		Tessellator.getInstance().draw();
+	}
 
-		double u = TEXTURE_OFFSETS[info.u];
-		double v = TEXTURE_OFFSETS[info.v];
+	private void drawIconVectors(double x, double y, double z, Side side, TextureManager tm) {
+		VertexBuffer vb;
+		tm.bindTexture(ICONS_TEXTURE);
+		vb = Tessellator.getInstance().getBuffer();
+		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
+		vb.setTranslation(-x, -y, -z);
+		for (SelectBlock select : selectedBlocks) {
+			if (select.valid) {
+				renderVectors(vb, select.pos, TEXTURE_OFFSETS[0], TEXTURE_OFFSETS[0], side, 1.001);
+			}
+		}
+		vb.setTranslation(0, 0, 0);
+		Tessellator.getInstance().draw();
+	}
+
+	private void renderVectors(VertexBuffer vb, BlockPos pos, double u, double v, Side side, double yOffset) {
+		double x = pos.getX();
+		double y = pos.getY() + yOffset;
+		double z = pos.getZ();
 
 		if (Side.WHITE.equals(side)) {
 			vector(vb, x, y, z, u, v, 0, 0, T, T);
@@ -147,5 +170,6 @@ public class CheckerBoardOverlay {
 	public static class SelectBlock {
 		public int u, v;
 		public BlockPos pos;
+		public boolean valid;
 	}
 }
