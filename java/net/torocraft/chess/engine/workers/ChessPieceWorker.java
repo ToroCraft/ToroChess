a9007cbfb3268525fc0e6d05e1c6eee864a420bd
Alexander Lemkin <alexanderlemkin@gmail.com>
2017-02-19 19:35:31 -0500

    Completedish Pawn worker rules to engine

--- java/net/torocraft/chess/engine/workers/ChessPieceWorker.java
+++ java/net/torocraft/chess/engine/workers/ChessPieceWorker.java
@@ -5,20 +5,46 @@ import net.torocraft.chess.engine.MoveResult;
 
 import java.util.List;
 
+import static net.torocraft.chess.engine.ChessPieceState.Position;
+
 public abstract class ChessPieceWorker implements IChessPieceWorker {
-    private MoveResult moveResult;
-    private final List<ChessPieceState> state;
-    private final ChessPieceState chessPieceToMove;
+    protected final List<ChessPieceState> state;
+    protected final ChessPieceState chessPieceToMove;
+    protected MoveResult moveResult;
+    protected ChessPieceState[][] positionArray;
 
     public ChessPieceWorker(List<ChessPieceState> state, ChessPieceState chessPieceToMove) {
         this.state = state;
         this.chessPieceToMove = chessPieceToMove;
         moveResult = new MoveResult();
+        positionArray = new ChessPieceState[8][8];
+        populatePositionArray();
     }
 
     @Override
-    public boolean isKingInCheck(List<ChessPieceState> updatedState) {
+    public boolean willPutKingInCheck(Position positionToMoveCurrentPieceTo) {
         //TODO, check updated state for king in check
-        return true;
+        return false;
+    }
+
+    private void populatePositionArray() {
+        for (ChessPieceState piece : state) {
+            positionArray[piece.position.rank.ordinal()][piece.position.file.ordinal()]
+                    = piece;
+        }
+    }
+
+    protected boolean isSpaceFree(Position position) {
+        return positionArray[position.rank.ordinal()][position.file.ordinal()]
+                == null;
+    }
+
+    protected boolean isEnemyOccupying(Position position) {
+        ChessPieceState pieceState = positionArray[position.rank.ordinal()][position.file.ordinal()];
+        return !(pieceState == null || !pieceState.side.equals(chessPieceToMove.side));
+    }
+
+    protected void addLegalMove(Position position) {
+        moveResult.legalPositions.add(position);
     }
 }
--- java/net/torocraft/chess/engine/workers/IChessPieceWorker.java
+++ java/net/torocraft/chess/engine/workers/IChessPieceWorker.java
@@ -3,9 +3,9 @@ package net.torocraft.chess.engine.workers;
 import net.torocraft.chess.engine.ChessPieceState;
 import net.torocraft.chess.engine.MoveResult;
 
-import java.util.List;
+import static net.torocraft.chess.engine.ChessPieceState.Position;
 
 public interface IChessPieceWorker {
-    MoveResult getLegalMoves(List<ChessPieceState> state, ChessPieceState chessPieceToMove);
-    boolean isKingInCheck(List<ChessPieceState> state);
+    MoveResult getLegalMoves();
+    boolean willPutKingInCheck(Position positionToMoveCurrentPieceTo);
 }
