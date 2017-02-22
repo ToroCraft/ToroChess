1a09abaa647139f142b895ccc64c1aa3ac5214e1
Alexander Lemkin <alexanderlemkin@gmail.com>
2017-02-21 20:44:07 -0500

    Implemented Knight worker logic

--- java/net/torocraft/chess/engine/chess/workers/KnightWorker.java
+++ java/net/torocraft/chess/engine/chess/workers/KnightWorker.java
@@ -3,6 +3,8 @@ package net.torocraft.chess.engine.chess.workers;
 import net.torocraft.chess.engine.chess.ChessPieceState;
 import net.torocraft.chess.engine.chess.ChessMoveResult;
 
+import static net.torocraft.chess.engine.GamePieceState.Position;
+
 import java.util.List;
 
 public class KnightWorker extends ChessPieceWorker {
@@ -12,7 +14,41 @@ public class KnightWorker extends ChessPieceWorker {
 
     @Override
     public ChessMoveResult getLegalMoves() {
-        //TODO get legal moves for this piece
-        return null;
+        Position chessPiecePosition = chessPieceToMove.position;
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()-2,
+                chessPiecePosition.file.ordinal()-1));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()-2,
+                chessPiecePosition.file.ordinal()+1));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()+1,
+                chessPiecePosition.file.ordinal()-2));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()+1,
+                chessPiecePosition.file.ordinal()+2));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()-1,
+                chessPiecePosition.file.ordinal()+2));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()-1,
+                chessPiecePosition.file.ordinal()-2));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()+2,
+                chessPiecePosition.file.ordinal()-1));
+
+        checkIfLegalMove(tryCreatePosition(chessPiecePosition.rank.ordinal()+2,
+                chessPiecePosition.file.ordinal()+1));
+
+        return moveResult;
+    }
+
+    private void checkIfLegalMove(Position positionToCheck) {
+        if (positionToCheck == null) {
+            return;
+        }
+        if(isSpaceFreeFullCheck(positionToCheck) || isEnemyOccupyingFullCheck(positionToCheck)){
+            addLegalMove(positionToCheck);
+        }
     }
 }
