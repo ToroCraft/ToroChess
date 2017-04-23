0b7558ebd4ed55996ed6582a48868b4a42024614
Charles Howard <frodare@gmail.com>
2017-04-23 19:08:44 -0400

    version step to 0.2

--- java/net/torocraft/chess/ToroChess.java
+++ java/net/torocraft/chess/ToroChess.java
@@ -15,7 +15,7 @@ public class ToroChess {
 
 	public static final String MODID = "torochess";
 	public static final String MODNAME = "ToroChess";
-	public static final String VERSION = "1.11.2-0.1";
+	public static final String VERSION = "1.11.2-0.2";
 
 	@Mod.Instance(MODID)
 	public static ToroChess INSTANCE;
